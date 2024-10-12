7593b91e5a13cc6556c891bee9edcb79f3c063bd
Azadcelik <azadcelikk12@gmail.com>
2024-10-12 00:01:04 -0400

    first

---
+++ 01intro/typesctipt/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Azad", age: 21, email: "asjndasjk@gmail.com" };
+console.log("welcome back to the js and be with the javascript forever hopefully");
+console.log(user.name);
---
+++ 01intro/typesctipt/intro.ts
@@ -0,0 +1,9 @@
+let user = {name: "Azad", age:21, email: "asjndasjk@gmail.com"}
+
+console.log("welcome back to the js and be with the javascript forever hopefully")
+
+console.log(user.what)
+
+//so typescript whole idea is save me from errors and again this is just a dev tool  development tool  wrapper around javascript making your 
+//life much easier 
+//error does not stop you producing your code 
