e71fd7c336be4c283c5a152dcad781d1d37439cd
Azadcelik <azadcelikk12@gmail.com>
2024-10-12 00:16:07 -0400

    variables

---
+++ 01intro/variables.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Azadiya welat";
+console.log(name);
+console.log(name.charAt(2));
---
+++ 01intro/variables.ts
@@ -0,0 +1,8 @@
+let name: string = "Azadiya welat";
+
+console.log(name);
+
+
+console.log(name.charAt(2))
+
+export {}
