ca58b28a6b3dfa22be36b9ead26463aab628abd9
Azadcelik <azadcelikk12@gmail.com>
2024-10-26 16:27:34 -0400

    arrays in typescripts

---
+++ 01intro/myArray.js
@@ -0,0 +1,5 @@
+var mentalDisorders = ["depression", "anxiety", "schizoprenia", "autism"];
+var numberArrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+mentalDisorders.push("bipolar disorder");
+mentalDisorders.pop();
+console.log(mentalDisorders);
---
+++ 01intro/myArray.ts
@@ -0,0 +1,9 @@
+const mentalDisorders: Array<string> = ["depression","anxiety", "schizoprenia","autism"]
+
+const numberArrays: Array<number> = [1,2,3,4,5,6,7,8,9,11]
+
+
+mentalDisorders.push("bipolar disorder")
+mentalDisorders.pop()
+console.log(mentalDisorders)
+
