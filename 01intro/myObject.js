2a9113bc45445f9177ad562e66ae6ba8b9ef099d
Azadcelik <azadcelikk12@gmail.com>
2024-11-04 21:39:31 -0500

    passing type instance object to the function

--- 01intro/myObject.js
+++ 01intro/myObject.js
@@ -1,12 +1,23 @@
 function createUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-createUser({ name: "azad", isPaid: true });
+// createUser({name: "azad", isPaid: true});
 function createCourse(_a) {
     var string = _a.studentName, number = _a.price;
     return { studentName: "Azad", price: 245335 };
 }
+//you usuaally pass object to the function or you have to return some obhects through the function
 function myTeamFunction(team) {
     console.log(team.color, team.price, team.championship);
 }
 myTeamFunction({ color: "Red", price: 1298323, championship: true });
+function myCompanyName(team) {
+    console.log(team, "how are you do bro");
+    return { color: "Blue", price: 123, championship: false };
+}
+var anotherTeam = {
+    color: "Orange",
+    price: 0,
+    championship: true
+};
+myCompanyName(anotherTeam);
--- 01intro/myObject.ts
+++ 01intro/myObject.ts
@@ -4,7 +4,7 @@
 function createUser({name: string, isPaid: boolean}) {    
 }
 
-createUser({name: "azad", isPaid: true});
+// createUser({name: "azad", isPaid: true});
 
 
 
@@ -22,7 +22,7 @@ type Team = {
 }
 
 
-
+//you usuaally pass object to the function or you have to return some obhects through the function
 function myTeamFunction(team: Team): void {
     console.log(team.color,team.price,team.championship)
 }
@@ -30,3 +30,19 @@ function myTeamFunction(team: Team): void {
 
 myTeamFunction({color: "Red",price: 1298323,championship: true})
 
+
+function myCompanyName(team:Team): {color:string,price:number,championship:boolean} {
+    console.log(team,"how are you do bro")
+    return {color: "Blue",price: 123,championship: false}
+
+}
+
+//you should create an instance of the Team type that your function expects as an argument
+const anotherTeam: Team = {
+    color: "Orange",
+    price: 0,
+    championship: true
+}
+
+
+myCompanyName(anotherTeam)
